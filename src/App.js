c1ad79f81e70d2fafe7886d1e754644bf2ae9874
Sonali-Chouhan <sonaliyuvasoft363@gmail.com>
2023-01-10 11:38:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import React,{useState} from "react"
+
+function App() {
+ const [state, setstate] = useState("")
+ const handleClick=()=>{
+  setstate("hello")
+ }
+  return (
+    <div className="App">
+      <h1>{state}</h1>
+    <button onClick={()=>handleClick()}>
+     click me
+    </button>
+    </div>
+  );
+}
+
+export default App;
