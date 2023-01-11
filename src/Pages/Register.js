9863ba02d2edfb148806a3c266a40531539f3986
Sonali-Chouhan <sonaliyuvasoft363@gmail.com>
2023-01-11 11:53:38 +0530

    add redux files

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
 import Button from "../Commoncomponent/CommonButton";
-// import Button from "../common/Button";
 import InputField from "../Commoncomponent/FormInput";
 import './index.css';
-// import '../App.css';
+
 function Register(){
+
     const [inputValue, setInputValue] = useState({
         name: "",
         price: ""
     })
+
     const handleChange = (input) => {
         setInputValue({ name: input.name, price: input.price })
     };
+
     const onHandleClick =()=>{
-console.log(767,inputValue)
+      console.log(767,inputValue)
     }
-    return(
+    
+return(
         <div className="container">
          Registration Form
          <form>
